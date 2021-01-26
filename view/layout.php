6a976427931f3e919192ec0d118dc0567acca895
Rodrigmen <roblesminambresrodrigo@gmail.com>
2021-01-26 13:46:38 +0100

    changes 1.0

--- view/layout.php
+++ view/layout.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>LoginLogoffMulticapaPOO</title>
+        <title>AplicacionFinal2021</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet"  href="webroot/css/estilos.css"       type="text/css" >
